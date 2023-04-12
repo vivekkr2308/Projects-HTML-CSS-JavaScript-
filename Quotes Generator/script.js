5737d0eb0f3c5e85e31da3f35e02dadb4011d595
Vivek Kumar <65390124+vivekkr2308@users.noreply.github.com>
2023-04-12 12:58:14 +0530

    Add files via upload

---
+++ Quotes Generator/script.js
@@ -0,0 +1,64 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.querySelector("span.loader");
+
+// Show Loading
+function loading() {
+  loader.classList.remove("hide");
+  quoteContainer.classList.add("hide");
+}
+
+// Hide Loading
+function complete() {
+  loader.classList.add("hide");
+  quoteContainer.classList.remove("hide");
+}
+
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+  // Pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+  // Check if Author field is blank and replace it with 'Unkown'
+  authorText.textContent = quote.author || "Unknown";
+
+  // Check if Quote length is long. If yes, apply .long-quote which reduces the font-size
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
+}
+
+// Get Quotes from API
+async function getQuotes() {
+  loading();
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+    complete();
+  } catch (error) {
+    // Catch error here
+  }
+}
+
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+
+// On load
+getQuotes();
